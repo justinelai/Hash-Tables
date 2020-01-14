d6272d61c8854b46305b01e361c283f95e8ee3ce
justinelai <justine.lai@gmail.com>
2020-01-14 18:23:55 -0500

    resize stretch

--- src/hashtable.py
+++ src/hashtable.py
@@ -12,7 +12,8 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
+        self.count = 0
+        self.doubled = False
 
     def _hash(self, key):
         return hash(key)
@@ -33,11 +34,26 @@ class HashTable:
     def _hash_mod(self, key):
         return self._hash_djb2(key) % self.capacity
 
+    def _load_factor(self):
+        load = self.count / self.capacity
+        if load >= 0.7:
+            self.doubled = True
+            self.resize(2)
+            print("Doubled!")
+        elif load <= 0.2 and self.doubled == True:
+            self.resize(0.5)
+            self.doubled = False
+            print("Shrinkage!")
+        else: 
+            pass 
 
     def insert(self, key, value):
         hashIndex = self._hash_mod(key)
         current = self.storage[hashIndex]
-        while current:
+        if current is None:
+            self.storage[hashIndex] = LinkedPair(key,value)
+            if self.doubled == True: return
+        else:
             if current.key == key:
                 current.value = value
                 return
@@ -45,24 +61,25 @@ class HashTable:
                 current = current.next
             else: 
                 current.next = LinkedPair(key, value)
-                return
-
-        self.storage[hashIndex] = LinkedPair(key,value)
-
-
-
+                if self.doubled == True: return
+        self.count += 1
+        self._load_factor()
+        
     def remove(self, key):
         hashIndex = self._hash_mod(key)
         current = self.storage[hashIndex]
         if current.key == key:
             self.storage[hashIndex] = current.next
+            self.count -= 1
+            self._load_factor()
             return
         while current:
             prev = current
             current = current.next
             if current.key == key:
                 prev.next = current.next
-                return
+                self.count -= 1
+                self._load_factor()
         else: 
             print("Warning! Key not found")
             return None
@@ -79,17 +96,17 @@ class HashTable:
         else: return None
 
 
-
-    def resize(self):
-        self.capacity = 2 * self.capacity
+    def resize(self, factor):
         prevStorage = self.storage
-        self.storage =  [None] * self.capacity
+        freezeCount = self.count
+        self.capacity = int(factor * self.capacity)
+        self.storage = [None] * self.capacity
         for i in prevStorage:
             while i:
                 self.insert(i.key, i.value)
                 i = i.next
+        self.count = freezeCount
         return
-
 
 if __name__ == "__main__":
     ht = HashTable(2)
